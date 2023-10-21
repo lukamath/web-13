e7ddafa9e0b0b3875ca65548ddf2bf9beefbd522
lukamath <lukamath@cella1.com>
2023-10-21 14:27:49 +0200

    redirect to countdown page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timedelta  # Import timedelta
 
@@ -39,12 +39,15 @@ def kaz13():
             error_message = "You've reached the limit of 10 words or 100 characters."
         else:
             new_message = Message(user=username, message=message_text, num_words=num_words, num_chars=num_chars, date_time=date_time)
+            start_time = int(date_time.timestamp())  
             db.session.add(new_message)
             db.session.commit()
+            #return render_template('countdown.html', start_time=start_time)  #this do not show the parameter, maybe to consider for something
+            return redirect(url_for('countdown', start_time=start_time))
 
     return render_template('index.html', error_message=error_message)
 
-#
+
 
 @app.route('/api/messages/<username>', methods=['GET'])
 def get_messages(username):
@@ -83,6 +86,12 @@ def get_message():
         return jsonify(message_data)
     else:
         return jsonify({'message': 'No message available for the next 10 minutes'})
+
+@app.route('/countdown/<int:start_time>')
+def countdown(start_time):
+    #start_time = int(date_time.timestamp())
+    return render_template('countdown.html', start_time=start_time)
+
 
 if __name__ == '__main__':
     with app.app_context():
