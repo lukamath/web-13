d94897abf65ca6821124028fdb3e3a5e8b11c2ac
lukamath <lukamath@cella1.com>
2023-10-22 18:58:03 +0200

    working on blocking slots

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, timedelta  # Import timedelta
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///message_app0.db'
@@ -13,15 +13,21 @@ class Message(db.Model):
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     num_words = db.Column(db.Integer)
     num_chars = db.Column(db.Integer)
-    date_time = db.Column(db.DateTime)  # Change the column type to DateTime
+    date_time = db.Column(db.DateTime)
+
+class Slot(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    unixtime = db.Column(db.Integer, unique=True)
+    message_id = db.Column(db.Integer, db.ForeignKey('message.id'))
 
 @app.route('/', methods=['GET', 'POST'])
 def kaz13():
     error_message = None
+
     if request.method == 'POST':
         username = request.form.get('username')
         message_text = request.form.get('input_text')
-        date_time_str = request.form.get('date_time')  # Get the date and time string
+        date_time_str = request.form.get('date_time')
 
         words = message_text.split()
         num_words = len(words)
@@ -29,7 +35,6 @@ def kaz13():
 
         try:
             date_time_str = date_time_str + ':00'
-            # Parse the date and time string into a datetime object
             date_time = datetime.strptime(date_time_str, "%Y-%m-%d %H:%M:%S")
         except ValueError:
             error_message = "Invalid date and time format."
@@ -37,17 +42,22 @@ def kaz13():
 
         if num_words > 10 or num_chars > 100:
             error_message = "You've reached the limit of 10 words or 100 characters."
+        elif Slot.query.filter_by(unixtime=int(date_time.timestamp())).first():
+            error_message = "The selected time slot is already taken."
         else:
             new_message = Message(user=username, message=message_text, num_words=num_words, num_chars=num_chars, date_time=date_time)
-            start_time = int(date_time.timestamp())  
+            start_time = int(date_time.timestamp())
             db.session.add(new_message)
             db.session.commit()
-            #return render_template('countdown.html', start_time=start_time)  #this do not show the parameter, maybe to consider for something
+
+            new_slot = Slot(unixtime=start_time, message_id=new_message.id)
+            db.session.add(new_slot)
+            db.session.commit()
+
             return redirect(url_for('countdown', start_time=start_time))
 
-    return render_template('index.html', error_message=error_message)
-
-
+    taken_slots = [slot.unixtime for slot in Slot.query.all()]
+    return render_template('index.html', error_message=error_message, taken_slots=taken_slots)
 
 @app.route('/api/messages/<username>', methods=['GET'])
 def get_messages(username):
@@ -66,23 +76,21 @@ def display_messages():
     messages = Message.query.order_by(Message.timestamp.desc()).all()
     return render_template('messages.html', messages=messages)
 
-@app.route('/w13')  #to dipaly messages in real time 
+@app.route('/w13')
 def w13():
     return render_template('w13.html')
 
-@app.route('/get_message', methods=['GET']) #   <-- w13.html
+@app.route('/get_message', methods=['GET'])
 def get_message():
     current_time = datetime.now()
     print("Current Time:", current_time)
-    # Query your database for the message where the time slot is within the next 10 minutes
-    # Replace 'your_query' with the appropriate query to find the message
-    message = db.session.query(Message).filter(Message.date_time > current_time -timedelta(minutes=10), Message.date_time <= current_time).first()
+    message = db.session.query(Message).filter(Message.date_time > current_time - timedelta(minutes=10), Message.date_time <= current_time).first()
     if message:
         message_data = {
             'user': message.user,
             'message': message.message
         }
-        print("Slot Time:", message.date_time)  # You can safely print message.date_time here
+        print("Slot Time:", message.date_time)
         return jsonify(message_data)
     else:
         return jsonify({'message': 'No message available for the next 10 minutes'})
@@ -99,6 +107,10 @@ def countdown(start_time):
 
     return render_template('countdown.html', start_time=start_time)
 
+@app.route('/api/taken_slots', methods=['GET'])
+def get_taken_slots():
+    taken_slots = Slot.query.all()
+    return jsonify([{"unixtime": slot.unixtime} for slot in taken_slots])
 
 if __name__ == '__main__':
     with app.app_context():
